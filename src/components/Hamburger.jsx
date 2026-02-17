fe821097185d8bb3bba10c56c4f6b01284c650fd
IsmailMan81F <ismailman81fmeg@gmail.com>
2026-02-17 23:42:20 +0100

    fix the animation of the hamburger menu

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -4,7 +4,7 @@ export default function Hamburger({ onClick, isOpen }) {
       onClick={() => {
         onClick();
       }}
-      className="w-6 h-4 flex flex-col justify-between md:hidden hover:cursor-pointer"
+      className="w-6 h-4 flex flex-col justify-between md:hidden"
     >
       <div
         className={`duration-300 h-0.75 w-6 bg-white rounded ${isOpen && "rotate-45 translate-y-[230%] w-6.5"}`}
